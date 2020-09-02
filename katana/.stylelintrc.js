a48d9748d4d2ad78fbcd31a5d453dae67cd62f9d
Tamnil Saito Junior <tamnil@gmail.com>
2020-09-01 22:27:57 -0300

    linters rc

---
+++ katana/.stylelintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    // extends: "stylelint-config-standard",
+    rules: {
+        // "string-quotes": "single",
+        // "block-no-empty": null,
+        // "comment-empty-line-before": [
+        //     "always",
+        //     {
+        //         ignore: ["stylelint-commands", "after-comment"]
+        //     }
+        // ],
+        // "declaration-colon-space-after": "always",
+        // indentation: [
+        //     4,
+        //     {
+        //         except: ["value"]
+        //     }
+        // ],
+        // "max-empty-lines": 2,
+        // "rule-empty-line-before": "always",
+        // "color-hex-case": "lower",
+        // "color-named": "always-where-possible",
+    }
+};
